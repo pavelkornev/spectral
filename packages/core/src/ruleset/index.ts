cfedd33c4445d90b1cd85d4b2fafbd96869166bd
Jakub Rożek <jakub@stoplight.io>
2022-01-31 12:24:28 +0100

    chore(core): setup package exports

--- packages/core/src/ruleset/index.ts
+++ packages/core/src/ruleset/index.ts
@@ -3,5 +3,5 @@ export { getDiagnosticSeverity } from './utils';
 export { createRulesetFunction, SchemaDefinition as RulesetFunctionSchemaDefinition } from './function';
 export { Format } from './format';
 export { RulesetDefinition, RuleDefinition, ParserOptions, HumanReadableDiagnosticSeverity } from './types';
-export { Ruleset } from './ruleset';
-export { Rule } from './rule';
+export { Ruleset, StringifiedRuleset } from './ruleset';
+export { Rule, StringifiedRule } from './rule';
